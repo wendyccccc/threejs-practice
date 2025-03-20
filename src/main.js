98095c134f11aa005c942976bd1894df19c35b45
Wendy Private <lienyun.chang@gmail.com>
2025-03-20 16:38:43 +0800

    feat: 移動貓頭鷹

--- src/main.js
+++ src/main.js
@@ -4,30 +4,73 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+let owl;
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(66, 66, 66)
 // PerspectiveCamera 透視相機(進大遠小)
 // 正交相機(沒有進大遠小)
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 50);
-camera.position.set(3, 0, 5);// 必須在render前設定
+camera.position.set(43, 0, 25);
 const renderer = new THREE.WebGLRenderer();
+// 陰影
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement)
-// 環境光
-const ambientLight = new THREE.AmbientLight({ color: 0xaaaaaa });
-scene.add(ambientLight)
 
-const directionLight = new THREE.DirectionalLight({ color: 0xffffff });
+
+const directionLight = new THREE.DirectionalLight(0xffffff, 1);
 scene.add(directionLight)
+directionLight.position.set(5, 15, 5);
+directionLight.castShadow = true;
 
+const SHADOW_DISTANCE = 20
+directionLight.shadow.camera.near = 0.1;
+directionLight.shadow.camera.far = SHADOW_DISTANCE;
+directionLight.shadow.camera.left = -SHADOW_DISTANCE;
+directionLight.shadow.camera.right = SHADOW_DISTANCE;
+directionLight.shadow.camera.top = SHADOW_DISTANCE;
+directionLight.shadow.camera.bottom = -SHADOW_DISTANCE;
+
+// const dirHelper = new THREE.DirectionalLightHelper(directionLight, 5);
+// scene.add(dirHelper);
+
+// const camHelper = new THREE.CameraHelper(directionLight.shadow.camera);
+// scene.add(camHelper);
+// const group = new THREE.Group();
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.position.set(5, 0, 0);
+scene.add(boxMesh);
+boxMesh.castShadow = true;
+boxMesh.receiveShadow = true;
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
 const gltfLoader = new GLTFLoader();
-gltfLoader.load('container.glb', (gltf) => {
+// gltfLoader.load('container.glb', (gltf) => {
+//     scene.add(gltf.scene);
+//     // gltf.scene.traverse((child) => {
+//     //     if (child.type === 'mesh') {
+//     //         child.castShadow = true;
+//     //         child.receiveShadow = true;
+//     //     }
+//     // })
+// })
+
+gltfLoader.load('owl.glb', (gltf) => {
     scene.add(gltf.scene);
+    owl = scene.getObjectByName('owl');
 })
+
+// scene.add(group);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 
 animation();
 // requestAnimationFrame比setInterval更有彈性 滑動更順暢
@@ -35,6 +78,10 @@ function animation() {
     requestAnimationFrame(animation);
     renderer.render(scene, camera);
     controls.update()
+
+    if (owl) {
+        owl.position.x -= 0.01;
+    }
 }
 
 
