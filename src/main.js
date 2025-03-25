1da09c4cb699f303aacb751905d5f8fc24655acb
Wendy Private <lienyun.chang@gmail.com>
2025-03-25 16:46:58 +0800

    feat: gsap 懸浮粒子 raycast

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import gsap from 'gsap'
+import { int } from 'three/tsl';
 let owl;
 
 const scene = new THREE.Scene();
@@ -44,7 +46,7 @@ directionLight.shadow.camera.bottom = -SHADOW_DISTANCE;
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.position.set(5, 0, 0);
+
 scene.add(boxMesh);
 boxMesh.castShadow = true;
 boxMesh.receiveShadow = true;
@@ -52,22 +54,91 @@ boxMesh.receiveShadow = true;
 const controls = new OrbitControls(camera, renderer.domElement)
 
 const gltfLoader = new GLTFLoader();
-// gltfLoader.load('container.glb', (gltf) => {
-//     scene.add(gltf.scene);
-//     // gltf.scene.traverse((child) => {
-//     //     if (child.type === 'mesh') {
-//     //         child.castShadow = true;
-//     //         child.receiveShadow = true;
-//     //     }
-//     // })
-// })
+
+const sphereType1 = new THREE.SphereGeometry(1);
+const sphereMat1 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+
+for (let i = 0; i < 10; i++) {
+    const sphere = new THREE.Mesh(sphereType1, sphereMat1);
+    scene.add(sphere);
+    sphere.position.setX(i * 3);
+    gsap.to(sphere.position,
+        { y: 30, duration: 2, repeat: -1, yoyo: true, ease: 'sine.inOut' }
+    )
+}
+
+const canRaycastMeshes = [];
+
+gltfLoader.load('container.glb', (gltf) => {
+    scene.add(gltf.scene);
+    gltf.scene.traverse((child) => {
+        if (child.type === 'mesh') {
+            child.castShadow = true;
+            child.receiveShadow = true;
+            canRaycastMeshes.push(child);
+        }
+    })
+})
 
 gltfLoader.load('owl.glb', (gltf) => {
     scene.add(gltf.scene);
     owl = scene.getObjectByName('owl');
+    gsap.to(owl.position, { x: 20, duration: 2, repeat: -1, yoyo: true, ease: 'sine.inOut' })
+    gsap.to(owl.rotation, { y: 360, duration: 30, repeat: -1, yoyo: true }).timeScale(0.1);
+
 })
 
-// scene.add(group);
+// 懸浮粒子
+const textureLoader = new THREE.TextureLoader();
+textureLoader.load('particle.png', (texture) => {
+    const vertices = [];
+
+    for (let i = 0; i < 1000; i++) {
+        const x = THREE.MathUtils.randFloatSpread(100);
+        const y = THREE.MathUtils.randFloatSpread(100);
+        const z = THREE.MathUtils.randFloatSpread(100);
+        vertices.push(x, y, z);
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    const material = new THREE.PointsMaterial({ color: 0xffffff, map: texture, transparent: true, size: 0.5 });
+    const points = new THREE.Points(geometry, material);
+
+    gsap.to(points.position, {
+        x: -8,
+        y: -3,
+        duration: 30,
+        repeat: -1,
+        yoyo: true,
+        ease: 'none',
+    })
+    scene.add(points);
+
+})
+
+let pointerScreen = new THREE.Vector2();
+let pointerThreejs = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+
+
+function checkRaycast() {
+    // 轉換座標
+    pointerThreejs.x = (pointerScreen / window.innerWidth) * 2 - 1;
+    pointerThreejs.y = -(pointerScreen / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(pointerThreejs, camera);
+    const intersects = raycaster.intersectObjects(canRaycastMeshes);
+    console.log(intersects)
+
+}
+
+window.addEventListener('mousemove', (e) => {
+    pointerThreejs.x = (e.clientX / window.innerWidth) * 2 - 1;
+    pointerThreejs.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    checkRaycast()
+})
+
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
@@ -78,10 +149,6 @@ function animation() {
     requestAnimationFrame(animation);
     renderer.render(scene, camera);
     controls.update()
-
-    if (owl) {
-        owl.position.x -= 0.01;
-    }
 }
 
 
