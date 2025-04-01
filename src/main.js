0708fe55a3b521192c36c152a67729ee1a13d412
Wendy Private <lienyun.chang@gmail.com>
2025-04-01 15:13:12 +0800

    raycast: 點擊後變色

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import gsap from 'gsap'
-import { int } from 'three/tsl';
+
 let owl;
 
 const scene = new THREE.Scene();
@@ -36,112 +36,158 @@ directionLight.shadow.camera.right = SHADOW_DISTANCE;
 directionLight.shadow.camera.top = SHADOW_DISTANCE;
 directionLight.shadow.camera.bottom = -SHADOW_DISTANCE;
 
-// const dirHelper = new THREE.DirectionalLightHelper(directionLight, 5);
-// scene.add(dirHelper);
-
-// const camHelper = new THREE.CameraHelper(directionLight.shadow.camera);
-// scene.add(camHelper);
-// const group = new THREE.Group();
-
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-
-scene.add(boxMesh);
-boxMesh.castShadow = true;
-boxMesh.receiveShadow = true;
-
 const controls = new OrbitControls(camera, renderer.domElement)
 
 const gltfLoader = new GLTFLoader();
 
-const sphereType1 = new THREE.SphereGeometry(1);
-const sphereMat1 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-
-
-for (let i = 0; i < 10; i++) {
-    const sphere = new THREE.Mesh(sphereType1, sphereMat1);
-    scene.add(sphere);
-    sphere.position.setX(i * 3);
-    gsap.to(sphere.position,
-        { y: 30, duration: 2, repeat: -1, yoyo: true, ease: 'sine.inOut' }
-    )
-}
 
 const canRaycastMeshes = [];
+let mixer01;
+// 按鍵觸發
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'f') {
+        mixer01 = new THREE.AnimationMixer(fireWorksGltf.scene);
+        const clips = fireWorksGltf.animations;
+        clips.forEach((clip) => {
+            const action = mixer01.clipAction(clip);
+            action.loop = THREE.LoopOnce;
+            action.clampWhenFinished = true;
+            action.play();
+        })
+    }
+})
 
-gltfLoader.load('container.glb', (gltf) => {
+
+let fireWorksGltf;
+gltfLoader.load('fireworks.glb', (gltf) => {
     scene.add(gltf.scene);
+    fireWorksGltf = gltf;
     gltf.scene.traverse((child) => {
-        if (child.type === 'mesh') {
-            child.castShadow = true;
-            child.receiveShadow = true;
+        if (child.isMesh) { // 只存入 Mesh
             canRaycastMeshes.push(child);
         }
     })
+
+    window.addEventListener('click', checkRaycast);
 })
 
 gltfLoader.load('owl.glb', (gltf) => {
     scene.add(gltf.scene);
     owl = scene.getObjectByName('owl');
+    // 可以把資料丟到userData裡面
+    owl.traverse((child) => {
+        child.userData.groupIns = owl;
+        if (child.isMesh) {
+            canRaycastMeshes.push(child);
+        }
+    });
+    console.log('owl', owl);
     gsap.to(owl.position, { x: 20, duration: 2, repeat: -1, yoyo: true, ease: 'sine.inOut' })
     gsap.to(owl.rotation, { y: 360, duration: 30, repeat: -1, yoyo: true }).timeScale(0.1);
 
 })
 
-// 懸浮粒子
-const textureLoader = new THREE.TextureLoader();
-textureLoader.load('particle.png', (texture) => {
-    const vertices = [];
-
-    for (let i = 0; i < 1000; i++) {
-        const x = THREE.MathUtils.randFloatSpread(100);
-        const y = THREE.MathUtils.randFloatSpread(100);
-        const z = THREE.MathUtils.randFloatSpread(100);
-        vertices.push(x, y, z);
-    }
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-    const material = new THREE.PointsMaterial({ color: 0xffffff, map: texture, transparent: true, size: 0.5 });
-    const points = new THREE.Points(geometry, material);
-
-    gsap.to(points.position, {
-        x: -8,
-        y: -3,
-        duration: 30,
-        repeat: -1,
-        yoyo: true,
-        ease: 'none',
+gltfLoader.load('cubes.glb', (gltf) => {
+    scene.add(gltf.scene);
+    gltf.scene.traverse((child) => {
+        if (child.name.indexOf('Cube') >= 0) {
+            canRaycastMeshes.push(child);
+        }
     })
-    scene.add(points);
-
 })
+
+// // 懸浮粒子
+// const textureLoader = new THREE.TextureLoader();
+// textureLoader.load('particle.png', (texture) => {
+//     const vertices = [];
+
+//     for (let i = 0; i < 1000; i++) {
+//         const x = THREE.MathUtils.randFloatSpread(100);
+//         const y = THREE.MathUtils.randFloatSpread(100);
+//         const z = THREE.MathUtils.randFloatSpread(100);
+//         vertices.push(x, y, z);
+//     }
+
+//     const geometry = new THREE.BufferGeometry();
+//     geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+//     const material = new THREE.PointsMaterial({ color: 0xffffff, map: texture, transparent: true, size: 0.5 });
+//     const points = new THREE.Points(geometry, material);
+
+//     gsap.to(points.position, {
+//         x: -8,
+//         y: -3,
+//         duration: 30,
+//         repeat: -1,
+//         yoyo: true,
+//         ease: 'none',
+//     })
+//     scene.add(points);
+
+// })
 
 let pointerScreen = new THREE.Vector2();
 let pointerThreejs = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
 
-
-function checkRaycast() {
+// 拾取step1: new raycaster
+function checkRaycast(e) {
     // 轉換座標
-    pointerThreejs.x = (pointerScreen / window.innerWidth) * 2 - 1;
-    pointerThreejs.y = -(pointerScreen / window.innerHeight) * 2 + 1;
+    pointerThreejs = {
+        x: (e.clientX / window.innerWidth) * 2 - 1,
+        y: -(e.clientY / window.innerHeight) * 2 + 1
+    };
+
+
     raycaster.setFromCamera(pointerThreejs, camera);
     const intersects = raycaster.intersectObjects(canRaycastMeshes);
-    console.log(intersects)
 
+    if (intersects[0]?.object.name === 'fireworks') {
+
+
+        // fireWorksGltf.scene.rotateY(30);
+
+        const fireworks = scene.getObjectByName('fireworks');
+        fireworks.rotateY(30)
+
+        mixer01 = new THREE.AnimationMixer(fireWorksGltf.scene);
+        const clips = fireWorksGltf.animations;
+        clips.forEach((clip) => {
+            const action = mixer01.clipAction(clip);
+            action.loop = THREE.LoopOnce;
+            action.clampWhenFinished = true;
+            action.play();
+        })
+
+    }
+
+    console.log('intersects', intersects);
+
+    if (intersects[0]?.object.name.indexOf('Cube') >= 0) {
+        console.log('cube', intersects[0].object.name);
+        const cube = intersects[0].object;
+
+        const newMaterial = cube.material.clone();
+        cube.material = newMaterial;
+        cube.material.color = new THREE.Color(0, 255, 0);
+    }
 }
+
+window.addEventListener('click', (e) => {
+    pointerThreejs.x = (e.clientX / window.innerWidth) * 2 - 1;
+    pointerThreejs.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    checkRaycast(e);
+});
+
 
 window.addEventListener('mousemove', (e) => {
     pointerThreejs.x = (e.clientX / window.innerWidth) * 2 - 1;
     pointerThreejs.y = -(e.clientY / window.innerHeight) * 2 + 1;
-    checkRaycast()
+    // checkRaycast(e)
 })
 
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
 
 animation();
 // requestAnimationFrame比setInterval更有彈性 滑動更順暢
@@ -149,6 +195,10 @@ function animation() {
     requestAnimationFrame(animation);
     renderer.render(scene, camera);
     controls.update()
+
+    if (mixer01) {
+        mixer01.update(0.02);
+    }
 }
 
 
